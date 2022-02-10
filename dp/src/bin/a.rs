89bff888b52eafb1a90f196bf0c608c72b3d6617
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-02-11 01:18:10 +0900

    5.1

--- dp/src/bin/a.rs
+++ dp/src/bin/a.rs
@@ -1,5 +1,17 @@
 use proconio::input;
+use std::cmp::min;
 
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        h: [isize; n]
+    }
+    let inf: isize = 1<<30;
+    let mut dp = vec![inf; n];
+    dp[0] = 0;
+    dp[1] = (h[1] - h[0]).abs();
+    for i in 2..n {
+        dp[i] = min(dp[i-2]+(h[i]-h[i-2]).abs(), dp[i-1]+(h[i]-h[i-1]).abs());
+    }
+    println!("{}", dp[n-1]);
 }

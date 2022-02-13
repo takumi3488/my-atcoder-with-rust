7c9966a664f15b944ba0516a358d240fbb3b290b
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-02-13 18:49:34 +0900

    [finish]dp/d

--- dp/src/bin/d.rs
+++ dp/src/bin/d.rs
@@ -1,5 +1,16 @@
 use proconio::input;
 
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        w: usize,
+        a: [(usize,usize);n]
+    }
+    let mut dp = vec![0; w + 1];
+    for (aw, av) in a {
+        for j in (aw..w).rev() {
+            dp[j] = dp[j].max(dp[j - aw] + av);
+        }
+    }
+    println!("{}", dp[w]);
 }

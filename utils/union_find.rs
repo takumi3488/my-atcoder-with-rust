bb1ae264cce8e4f8ff16397910c33ad25540ef7f
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-02-15 23:19:13 +0900

    [add]groups_count function

--- utils/union_find.rs
+++ utils/union_find.rs
@@ -6,23 +6,26 @@ struct UnionFind {
 impl UnionFind {
     fn new(n: usize) -> Self {
         UnionFind {
-            par: (0..n).collect(),
-            siz: vec![1; n],
+            par: vec![-1; n], // 親を返す(いなければ-1を返す)
+            siz: vec![1; n], // 根がiのグループのサイズをsiz[i]に格納
         }
     }
 
+    // 根を返す
     fn root(&mut self, x: usize) -> usize {
-        if self.par[x] == x {
+        if self.par[x] ==-1 {
             return x;
         }
         self.par[x] = self.root(self.par[x]);
         self.par[x]
     }
 
+    // 同じ根を持つかを判別
     fn issame(&mut self, x: usize, y: usize) -> bool {
         self.root(x) == self.root(y)
     }
 
+    // 指定した2要素を含むグループを結合
     fn unite(&mut self, mut parent: usize, mut child: usize) -> bool {
         parent = self.root(parent);
         child = self.root(child);
@@ -40,8 +43,18 @@ impl UnionFind {
         true
     }
 
+    // その要素を含むグループの要素数をカウント
     fn size(&mut self, x: usize) -> usize {
         let root = self.root(x);
         self.siz[root]
+    }
+
+    // グループ数をカウント
+    fn groups_count(&mut self) -> usize {
+        let mut res = 0;
+        for p in self.par {
+            res += p==-1;
+        }
+        res
     }
 }

8fa0f63bc2031ec1075b92ea577aed6d15aa1a72
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-02-26 22:47:33 +0900

    .

--- abc241/src/bin/e.rs
+++ abc241/src/bin/e.rs
@@ -1,5 +1,14 @@
 use proconio::input;
 
 fn main() {
-    unimplemented!();
+    input!{
+        n: usize,
+        k: usize,
+        a: [usize; n]
+    }
+    let mut x = 0;
+    for _ in 0..k {
+        x += a[x%n];
+    }
+    println!("{}",x)
 }

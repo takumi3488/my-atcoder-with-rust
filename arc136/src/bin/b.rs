d3333f8656b4a352ede3a5d6578ad968e16f95aa
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-03-04 21:47:53 +0900

    .

--- arc136/src/bin/b.rs
+++ arc136/src/bin/b.rs
@@ -1,5 +1,16 @@
 use proconio::input;
 
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+        mut b: [usize; n]
+    }
+    a.sort_unstable();
+    b.sort_unstable();
+    if a == b {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
 }
--- arc136/src/bin/d.rs
+++ arc136/src/bin/d.rs
@@ -1,5 +1,28 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [String; n]
+    }
+    let mut dp = vec![vec![0;6];10];
+    let nums_all = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let mut nums = HashMap::new();
+    for i in 0..=9 {
+        hm.insert(i, 0);
+        nums.insert(i, &nums_all[0..=i]);
+    }
+    let mut res = 0;
+    for i in (0..n).rev() {
+        let mut k = 0;
+        for ai in a[i].chars().rev() {
+            let b = ai as usize - 48;
+            for j in *nums.get(&(9 - b)).unwrap() {
+                res += hm.get(j).unwrap();
+            }
+            hm.insert(b, hm.get(&b).unwrap() + 1);
+        }
+    }
+    println!("{}", res)
 }

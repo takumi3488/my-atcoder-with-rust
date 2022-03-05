8ec41fe28c98880f76def2cc0e1332e58a950838
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-03-05 21:16:12 +0900

    .

---
+++ abc242/src/bin/b.rs
@@ -0,0 +1,8 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        s: String
+    }
+    let res = s.chars().sort
+}
--- new.sh
+++ new.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-read -p "Project id: " id
-cargo atcoder new $id
+read -p "Project id: " id && \
+cargo atcoder new $id && \
 python3 add_packages.py $id

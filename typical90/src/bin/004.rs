ffc211481744dbe288f5b89ae8e1a99578ff5eda
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-03-10 22:38:05 +0900

    [finish]typical90/004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,5 +1,24 @@
 use proconio::input;
 
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+        a: [[usize; w]; h]
+    }
+    let mut sh = vec![0; h];
+    let mut vh = vec![0; w];
+    for i in 0..h {
+        for j in 0..w {
+            let aij = a[i][j];
+            sh[i] += aij;
+            vh[j] += aij;
+        }
+    }
+    for i in 0..h {
+        for j in 0..w {
+            print!("{} ", sh[i] + vh[j] - a[i][j]);
+        }
+        println!();
+    }
 }

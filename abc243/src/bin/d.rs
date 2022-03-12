8981b6b7631e5716d22f2827bb7376330c0463d7
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-03-13 00:25:10 +0900

    finish

--- abc243/src/bin/d.rs
+++ abc243/src/bin/d.rs
@@ -1,20 +1,28 @@
-use num::BigInt;
 use proconio::input;
+use std::collections::VecDeque;
 
 fn main() {
     input! {
         _: usize,
-        mut x: BigInt,
+        x: usize,
         s: String
     }
+    let b = format!("{:b}", x);
+    let mut res: VecDeque<char> = b.chars().into_iter().collect();
     for si in s.chars() {
         if si.to_string() == String::from("U") {
-            x = x / 2;
-        } else if si.to_string() == String::from("R") {
-            x = x * 2 + 1;
+            res.pop_back();
+        } else if si.to_string() == String::from("L") {
+            res.push_back("0".chars().next().unwrap());
         } else {
-            x = x * 2;
+            res.push_back("1".chars().next().unwrap());
         }
     }
-    println!("{}", x)
+    let mut r: usize = 0;
+    for (i, c) in res.iter().rev().enumerate() {
+        if c.to_string() == String::from("1") {
+            r += 2_usize.pow(i as u32);
+        }
+    }
+    println!("{}", r);
 }

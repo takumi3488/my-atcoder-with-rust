359c3d16d8ecd7df66e5f0d9c7a7114a1942ff75
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-03-20 19:03:06 +0900

    [finish]typical90/008

--- typical90/src/bin/008.rs
+++ typical90/src/bin/008.rs
@@ -1,5 +1,19 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
-    unimplemented!();
+    input! {
+        _n: usize,
+        s: Chars
+    }
+    let m = 1_000_000_007;
+    let t = "atcoder";
+    let mut dp = vec![0; 8];
+    dp[0] = 1;
+    for s in s {
+        if let Some(p) = t.find(s) {
+            dp[p + 1] = (dp[p] + dp[p + 1]) % m;
+        }
+    }
+    println!("{}", dp[7]);
 }

553272f35359de2c64e744f573a91b3cbee2eef4
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-03-20 18:32:42 +0900

    [finish]typical90/007

--- typical90/src/bin/007.rs
+++ typical90/src/bin/007.rs
@@ -1,5 +1,23 @@
 use proconio::input;
+use std::convert::identity;
 
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [isize; n],
+        q: usize,
+        b: [isize; q]
+    }
+    a.sort_unstable();
+    for &b in &b {
+        let i = a.binary_search(&b).unwrap_or_else(identity);
+        let ans = if i == 0 {
+            a[i] - b
+        } else if i == a.len() {
+            b - a[i - 1]
+        } else {
+            (a[i] - b).min(b - a[i - 1])
+        };
+        println!("{}", ans);
+    }
 }

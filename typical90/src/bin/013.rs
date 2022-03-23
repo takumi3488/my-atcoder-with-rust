8cc7ca56ce523f998b99ca2da3e5d0abb86e1afc
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-03-24 04:06:20 +0900

    [finish]typical90/013

--- typical90/src/bin/013.rs
+++ typical90/src/bin/013.rs
@@ -1,6 +1,17 @@
-use proconio::{input,fastout};
+use petgraph::{algo::dijkstra, graph::UnGraph};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32,
+        m: usize,
+        edges: [(u32,u32,usize);m]
+    }
+    let g = UnGraph::<(), usize>::from_edges(edges);
+    let from_1 = dijkstra(&g, 1.into(), None, |x| *x.weight());
+    let from_n = dijkstra(&g, n.into(), None, |x| *x.weight());
+    for i in 1..=n {
+        println!("{}", from_1[&i.into()] + from_n[&i.into()]);
+    }
 }

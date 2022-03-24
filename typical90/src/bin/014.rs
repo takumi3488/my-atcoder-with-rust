cfc7cbf262b269e13d289c71d539e6a060abb5cd
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-03-24 21:41:27 +0900

    [finish]typical90/014

--- typical90/src/bin/014.rs
+++ typical90/src/bin/014.rs
@@ -1,6 +1,17 @@
-use proconio::{input,fastout};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [i64; n],
+        mut b: [i64; n]
+    }
+    a.sort_unstable();
+    b.sort_unstable();
+    let mut res = 0;
+    for i in 0..n {
+        res += (a[i] - b[i]).abs();
+    }
+    println!("{}", res);
 }

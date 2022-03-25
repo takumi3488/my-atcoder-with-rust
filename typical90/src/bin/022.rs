e1b90d3198f9696facb1a2f3468d3f36d1bfb58a
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-03-25 10:36:07 +0900

    終了

--- typical90/src/bin/022.rs
+++ typical90/src/bin/022.rs
@@ -1,6 +1,13 @@
-use proconio::{input,fastout};
+use num::integer::gcd;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        a: usize,
+        b: usize,
+        c: usize
+    }
+    let g = gcd(a, gcd(b, c));
+    println!("{}", (a + b + c) / g - 3);
 }

0c5ed4e5df747e33358c2de5bea2b0491939ceb5
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-03-25 10:18:10 +0900

    終了

--- typical90/src/bin/021.rs
+++ typical90/src/bin/021.rs
@@ -1,6 +1,17 @@
-use proconio::{input,fastout};
+use petgraph::{algo::kosaraju_scc, graph::Graph};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        _: usize,
+        m: usize,
+        edges: [(u32, u32); m]
+    }
+    let g = Graph::<(), ()>::from_edges(&edges);
+    let v = kosaraju_scc(&g);
+    println!(
+        "{}",
+        v.iter().map(|x| x.len() * (x.len() - 1) / 2).sum::<usize>()
+    )
 }

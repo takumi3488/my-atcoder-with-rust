0dee1a04dbbba43ce2d4ebd3db53abf06d82b5e4
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-03-25 10:46:38 +0900

    終了

--- typical90/src/bin/024.rs
+++ typical90/src/bin/024.rs
@@ -1,6 +1,17 @@
-use proconio::{input,fastout};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: isize,
+        a: [isize; n],
+        b: [isize; n]
+    }
+    let d = a.iter().zip(b.iter()).map(|x| (x.0 - x.1).abs()).sum::<isize>();
+    if d <= k && d % 2 == k % 2 {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
 }

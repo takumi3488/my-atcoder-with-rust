604130ca27b54a535ec51a10baf199e4ac3de6a4
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-03-25 12:32:10 +0900

    終了

--- typical90/src/bin/027.rs
+++ typical90/src/bin/027.rs
@@ -1,6 +1,17 @@
-use proconio::{input,fastout};
+use proconio::{fastout, input};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: [String;n]
+    }
+    let mut hs = HashSet::new();
+    for i in 0..n {
+        if !hs.contains(&s[i]) {
+            hs.insert(&s[i]);
+            println!("{}", i + 1);
+        }
+    }
 }

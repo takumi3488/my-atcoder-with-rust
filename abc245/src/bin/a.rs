e096c1a19e91ab409bf037b29fedd5c421071591
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-03-26 21:03:24 +0900

    完了

--- abc245/src/bin/a.rs
+++ abc245/src/bin/a.rs
@@ -2,5 +2,19 @@ use proconio::{input,fastout};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize
+    }
+    if a < c {
+        println!("Takahashi")
+    } else if a > c {
+        println!("Aoki")
+    } else if b>d{
+        println!("Aoki")
+    } else {
+        println!("Takahashi")
+    }
 }

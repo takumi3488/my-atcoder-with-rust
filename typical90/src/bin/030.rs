cd07ecabdfcc57ba75418e39213a824921396f63
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-04-04 18:35:43 +0900

    完了

--- typical90/src/bin/030.rs
+++ typical90/src/bin/030.rs
@@ -1,6 +1,20 @@
-use proconio::{input,fastout};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize
+    }
+    let mut res = vec![0; n + 1];
+    for i in 2..=n {
+        if res[i] == 0 {
+            let mut j = i;
+            while j <= n {
+                res[j] += 1;
+                j += i;
+            }
+        }
+    }
+    println!("{}", res.iter().filter(|&x| x >= &k).count());
 }

ea2271cc5a18a56c5dbbdd525a99a580bd6c79a8
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-04-04 18:50:25 +0900

    完了

--- typical90/src/bin/033.rs
+++ typical90/src/bin/033.rs
@@ -1,6 +1,14 @@
-use proconio::{input,fastout};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize
+    }
+    if h == 1 || w == 1 {
+        println!("{}", h * w);
+    } else {
+        println!("{}", ((h + 1) / 2) * ((w + 1) / 2));
+    }
 }

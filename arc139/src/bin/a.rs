09153aae40b9aa6d0ac7786b8608a8d968f51ac2
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-04-24 22:13:10 +0900

    'Updated at: 2022/04/24 22:13:10'

--- arc139/src/bin/a.rs
+++ arc139/src/bin/a.rs
@@ -1,6 +1,20 @@
-use proconio::{input,fastout};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut t: [u32;n]
+    }
+    let mut a = vec![0; n];
+    a[0] = 2usize.pow(t[0]);
+    for i in 1..n {
+        let mut j = t[i] as usize;
+        a[i] = a[i - 1] & !(2usize.pow(t[i]) - 1);
+        if a[i] & 1 << j > 0 {
+            j += 1;
+        }
+        a[i] += 1 << j;
+    }
+    println!("{}", a[n - 1]);
 }

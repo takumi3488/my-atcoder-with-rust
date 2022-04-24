b7a2c86242d996e00b6f278eb7ff2e068b23e0ec
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-04-25 03:56:33 +0900

    'Updated at: 2022/04/25 03:56:33'

--- abc239/src/bin/d.rs
+++ abc239/src/bin/d.rs
@@ -1,6 +1,28 @@
-use proconio::{input,fastout};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize
+    }
+    let mut v: Vec<usize> = (2..=b + d).collect();
+    for i in 2..=((b + d) as f64).sqrt() as usize {
+        let mut j = 2;
+        while j * i <= b + d {
+            if let Ok(x) = v.binary_search(&(j * i)) {
+                v.remove(x);
+            }
+            j += 1;
+        }
+    }
+    for i in a..=b {
+        if (c..=d).all(|j| v.binary_search(&(i + j)).is_err()) {
+            println!("Takahashi");
+            return;
+        }
+    }
+    println!("Aoki");
 }

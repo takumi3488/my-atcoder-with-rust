99b2b2d64f7fa221b7cf63effcf7b299d69972ad
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-04-25 01:11:49 +0900

    'Updated at: 2022/04/25 01:11:49'

--- abc246/src/bin/c.rs
+++ abc246/src/bin/c.rs
@@ -1,6 +1,27 @@
-use proconio::{input,fastout};
+use proconio::{fastout, input};
+use std::cmp::min;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut k: usize,
+        x: usize,
+        mut a: [usize; n]
+    }
+    for i in 0..n {
+        let mk = min(k, a[i] / x);
+        a[i] -= mk * x;
+        k -= mk;
+        if k == 0 {
+            break;
+        }
+    }
+    a.sort_unstable();
+    if k > 0 {
+        for i in (if n > k { n - k } else { 0 }..n).rev() {
+            a[i] = 0;
+        }
+    }
+    println!("{}", a.iter().sum::<usize>());
 }

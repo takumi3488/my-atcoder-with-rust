b6a1dfa702feec53b4ad9ad0cecdbd6763182c9d
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-02 03:13:50 +0900

    'Updated at: 2022/05/02 03:13:50'

--- abc237/src/bin/c.rs
+++ abc237/src/bin/c.rs
@@ -1,6 +1,24 @@
-use proconio::{input,fastout};
+use std::collections::VecDeque;
+
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        mut s: Chars
+    }
+    let mut t = VecDeque::from(s);
+    while t.back() == Some(&'a') {
+        if t.front() == Some(&'a') {
+            t.pop_front();
+        }
+        t.pop_back();
+    }
+    for i in 0..t.len() / 2 {
+        if t[i] != t[t.len() - i - 1] {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
--- abc237/src/bin/d.rs
+++ abc237/src/bin/d.rs
@@ -1,6 +1,23 @@
-use proconio::{input,fastout};
+use proconio::{fastout, input, marker::Chars};
+use std::collections::VecDeque;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: Chars
+    }
+    let mut a = VecDeque::new();
+    a.push_front(n);
+    for i in (1..=n).rev() {
+        if s[i - 1] == 'L' {
+            a.push_back(i - 1);
+        } else {
+            a.push_front(i - 1);
+        }
+    }
+    for ai in a {
+        print!("{} ", ai);
+    }
+    println!();
 }

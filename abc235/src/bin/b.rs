03d7d69eb632211ae48b6b8bd2429a7c8f396112
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-04 17:00:36 +0900

    'Updated at: 2022/05/04 17:00:36'

--- abc235/src/bin/b.rs
+++ abc235/src/bin/b.rs
@@ -2,5 +2,17 @@ use proconio::{input,fastout};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        h: [usize; n]
+    }
+    let mut res = h[0];
+    for i in 0..n-1 {
+        if h[i] < h[i+1] {
+            res = h[i+1];
+        } else {
+            break;
+        }
+    }
+    println!("{}", res);
 }

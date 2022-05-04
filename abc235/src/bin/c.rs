41d294347cb0c1672ec26a42ef740e327da4bd43
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-04 17:31:08 +0900

    'Updated at: 2022/05/04 17:31:08'

--- abc235/src/bin/c.rs
+++ abc235/src/bin/c.rs
@@ -1,6 +1,32 @@
-use proconio::{input,fastout};
+use proconio::{fastout, input, marker::Usize1};
+use std::collections::HashMap;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        qn: usize,
+        a: [usize; n],
+        xk: [(usize,Usize1); qn]
+    }
+    let mut hm: HashMap<usize, Vec<usize>> = HashMap::new();
+    for i in 0..n {
+        if let Some(x) = hm.get_mut(&a[i]) {
+            (*x).push(i + 1);
+        } else {
+            hm.insert(a[i], vec![i + 1]);
+        }
+    }
+    for q in xk {
+        let len = if let Some(x) = hm.get(&q.0) {
+            x.len()
+        } else {
+            0
+        };
+        if len > q.1 {
+            println!("{}", hm.get(&q.0).unwrap()[q.1]);
+        } else {
+            println!("-1");
+        }
+    }
 }

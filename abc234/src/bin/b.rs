427376b1893cd7fe09fa6bd7e803947ab4afbbe0
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-06 00:11:31 +0900

    'Updated at: 2022/05/06 00:11:31'

--- abc234/src/bin/b.rs
+++ abc234/src/bin/b.rs
@@ -1,6 +1,15 @@
-use proconio::{input,fastout};
+use itertools::Itertools;
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        p: [(isize,isize);n]
+    }
+    let mut res = 0f64;
+    for q in p.iter().combinations(2) {
+        res = res.max((((q[0].0 - q[1].0).pow(2) + (q[0].1 - q[1].1).pow(2)) as f64).sqrt());
+    }
+    println!("{}", res);
 }

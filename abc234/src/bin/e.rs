6983632ed7330d00228aff53e5af874b87daacd3
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-06 01:19:55 +0900

    'Updated at: 2022/05/06 01:19:55'

--- abc234/src/bin/e.rs
+++ abc234/src/bin/e.rs
@@ -1,6 +1,29 @@
-use proconio::{input,fastout};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        x:i64
+    }
+    let mut c = vec![];
+    for i in 1..10 {
+        c.push(i);
+        for d in -9..10 {
+            let mut s = i;
+            let mut p = i;
+            while s < 10i64.pow(17) && 0 <= p + d && p + d <= 9 {
+                s *= 10;
+                p += d;
+                s += p;
+                c.push(s);
+            }
+        }
+    }
+    c.sort();
+    for i in c {
+        if i >= x {
+            println!("{}", i);
+            return;
+        }
+    }
 }

645c07e3e9431cc845f33310f124c065be97acbc
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-06 01:40:45 +0900

    'Updated at: 2022/05/06 01:40:45'

--- abc233/src/bin/b.rs
+++ abc233/src/bin/b.rs
@@ -1,6 +1,16 @@
-use proconio::{input,fastout};
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        l: Usize1,
+        r: usize,
+        s: Chars
+    }
+    print!("{}", &s[..l].iter().collect::<String>());
+    print!("{}", s[l..r].iter().rev().collect::<String>());
+    println!("{}", &s[r..].iter().collect::<String>());
 }

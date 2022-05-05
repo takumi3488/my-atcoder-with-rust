6000022b561258fad35e792e560b0e97e9dc7c7b
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-06 00:59:05 +0900

    'Updated at: 2022/05/06 00:59:05'

--- abc234/src/bin/d.rs
+++ abc234/src/bin/d.rs
@@ -1,6 +1,21 @@
-use proconio::{input,fastout};
+use std::collections::BinaryHeap;
+
+use proconio::{fastout, input, marker::Usize1};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: Usize1,
+        p: [isize; n]
+    }
+    let mut bh = BinaryHeap::new();
+    let mut res = 0;
+    for i in 0..n {
+        bh.push(-p[i]);
+        if i >= k {
+            res = res.max(-bh.pop().unwrap());
+            println!("{}", res);
+        }
+    }
 }

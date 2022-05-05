dfc2b669db98f9afdf1cdda22798d0204a691d75
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-06 02:41:51 +0900

    'Updated at: 2022/05/06 02:41:51'

--- abc233/src/bin/c.rs
+++ abc233/src/bin/c.rs
@@ -1,6 +1,14 @@
-use proconio::{input,fastout};
-
-#[fastout]
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        n: u128,
+        x: u128,
+        a: [[u128]; n]
+    };
+    let mut dp = vec![1_u128];
+
+    for ai in a.iter() {
+        dp = dp.iter().flat_map(|&i| ai.iter().map(move |&j| i * j)).collect();
+    }
+
+    println!("{:}", dp.iter().filter(|&i| *i == x).count());
 }

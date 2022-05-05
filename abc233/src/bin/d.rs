6b91b7fd44244a4f8dd6c4955abda551f162a3ca
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-06 02:56:59 +0900

    'Updated at: 2022/05/06 02:56:59'

--- abc233/src/bin/d.rs
+++ abc233/src/bin/d.rs
@@ -1,6 +1,20 @@
-use proconio::{input,fastout};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: isize,
+        a: [isize; n]
+    }
+    let mut res = 0usize;
+    let mut hm = std::collections::HashMap::new();
+    let mut s = 0;
+    for a in a {
+        *hm.entry(s).or_insert(0) += 1;
+        s += a;
+        res += *hm.get(&(s - k)).unwrap_or(&0);
+    }
+
+    println!("{}", res);
 }

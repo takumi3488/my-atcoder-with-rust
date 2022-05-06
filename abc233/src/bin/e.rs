a45758a793fb09e9ceec7fbc2acca5403fde3b5d
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-07 00:35:19 +0900

    'Updated at: 2022/05/07 00:35:19'

--- abc233/src/bin/e.rs
+++ abc233/src/bin/e.rs
@@ -1,6 +1,24 @@
-use proconio::{input,fastout};
+use proconio::{input, fastout};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        x: String
+    }
+    let n = x.len();
+    let x = x.as_bytes();
+    let mut d = vec![0;n];
+    let mut t = 0u32;
+    for i in 0..n {
+        t += (x[i] - b'0')as u32;
+        d[i] = t;
+    }
+    for i in (1..n).rev() {
+        d[i-1] += d[i]/10;
+        d[i] %= 10;
+    }
+    for i in 0..n {
+        print!("{}", d[i]);
+    }
+    println!();
 }

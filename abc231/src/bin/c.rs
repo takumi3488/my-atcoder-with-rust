9124fe32d1c068e734b34f79a80e310bc4224b01
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-07 03:06:28 +0900

    'Updated at: 2022/05/07 03:06:28'

--- abc231/src/bin/c.rs
+++ abc231/src/bin/c.rs
@@ -1,6 +1,15 @@
-use proconio::{input,fastout};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        q: usize,
+        mut a: [usize; n],
+        x: [usize; q]
+    }
+    a.sort_unstable();
+    for x in x {
+        println!("{}", n - a.binary_search(&x).unwrap_or_else(|y| y));
+    }
 }

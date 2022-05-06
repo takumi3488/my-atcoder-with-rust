2345108cb276ec3cd6b0578ec2d30277c2bf3801
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-07 03:52:47 +0900

    'Updated at: 2022/05/07 03:52:47'

--- abc231/src/bin/d.rs
+++ abc231/src/bin/d.rs
@@ -1,6 +1,22 @@
-use proconio::{input,fastout};
+use petgraph::unionfind::UnionFind;
+use proconio::{fastout, input, marker::Usize1};
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(Usize1,Usize1);m]
+    }
+    let mut v = vec![0; n + 1];
+    let mut uf = UnionFind::new(n);
+    for ab in ab {
+        v[ab.0] += 1;
+        v[ab.1] += 1;
+        if v[ab.0] > 2 || v[ab.1] > 2 || !uf.union(ab.0, ab.1) {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }

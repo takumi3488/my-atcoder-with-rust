04e89042aca319abe0415f46a883ef0348ee1b9a
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-07 02:39:28 +0900

    'Updated at: 2022/05/07 02:39:28'

--- abc232/src/bin/d.rs
+++ abc232/src/bin/d.rs
@@ -1,6 +1,18 @@
-use proconio::{input,fastout};
+use proconio::input;
 
-#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize, w: usize,
+        c: [String; h],
+    }
+    let c: Vec<Vec<char>> = c.iter().map(|c| c.chars().collect()).collect();
+    let mut dp = vec![vec![0; w+1]; h+1];
+    for i in (0..h).rev() {
+        for j in (0..w).rev() {
+            if c[i][j] == '.' {
+                dp[i][j] = dp[i+1][j].max(dp[i][j+1]) + 1;
+            }
+        }
+    }
+    println!("{}", dp[0][0]);
 }

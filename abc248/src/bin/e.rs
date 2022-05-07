f864a46c64aa4f40d3f601bf5f933fa868666f3d
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-08 01:02:10 +0900

    'Updated at: 2022/05/08 01:02:10'

---
+++ abc248/src/bin/e.rs
@@ -0,0 +1,12 @@
+use proconio::{input,fastout};
+use itertools::Itertools;
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+        p: [(isize,isize); n]
+    }
+    
+}

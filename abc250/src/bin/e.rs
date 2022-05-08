15dfa915e5f9a1bb2b1cd43859593ada78f5a922
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-08 22:42:50 +0900

    'Updated at: 2022/05/08 22:42:50'

--- abc250/src/bin/e.rs
+++ abc250/src/bin/e.rs
@@ -1,4 +1,5 @@
-use proconio::{input,fastout};
+use proconio::{input,fastout, marker::Usize1};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
@@ -7,7 +8,23 @@ fn main() {
         a: [usize; n],
         b: [usize; n],
         q: usize,
-        xy: [(usize,usize);q]
+        xy: [(Usize1,Usize1);q]
     }
-    let 
+    let mut av = vec![HashSet::new()];
+    av[0].insert(a[0]);
+    let mut bv = vec![HashSet::new()];
+    bv[0].insert(b[0]);
+    for i in 1..n {
+        av.push(av[i-1].clone());
+        av[i].insert(a[i]);
+        bv.push(bv[i-1].clone());
+        bv[i].insert(b[i]);
+    }
+    for xy in xy {
+        if av[xy.0].symmetric_difference(&bv[xy.1]).count() > 0 {
+            println!("No");
+        } else {
+            println!("Yes");
+        }
+    }
 }

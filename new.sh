e06b470ffea908c10723ae3bb394723d294fa019
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-05-14 02:44:24 +0900

    'Updated at: 2022/05/14 02:44:24'

--- new.sh
+++ new.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
-read "id?Project id: " && \
-cargo atcoder new $id && \
-python3 add_packages.py $id
+if [[ $1 =~ "^[0-9]+$" ]] then
+  cargo atcoder new "abc$1" && \
+  python3 add_packages.py "abc$1"
+else
+  cargo atcoder new $1 && \
+  python3 add_packages.py $1
+fi

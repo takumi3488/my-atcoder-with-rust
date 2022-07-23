f7031152e941cbbb0e8ae1a5337e1a37a56bae4c
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-07-24 04:27:59 +0900

    'Updated at: 2022/07/24 04:27:59'

--- new.sh
+++ new.sh
@@ -1,9 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
-if [[ $1 =~ "^[0-9]+$" ]] then
-  cargo atcoder new "abc$1" && \
-  python3 add_packages.py "abc$1"
-else
-  cargo atcoder new $1 && \
-  python3 add_packages.py $1
-fi
+cargo atcoder new $1 && \
+python3 add_packages.py $1
--- test.jl
+++
@@ -1,8 +0,0 @@
-function main()
-  n,k = readline() |> split .|> x -> parse(Int16,x)
-  p = []
-  for i in 1:n
-    p
-  end
-end
-main()
